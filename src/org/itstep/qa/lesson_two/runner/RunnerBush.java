9acac28cf9505b9c80c151fed5639d02b6ff6b7b
mike <mike@gmail.com>
2018-10-08 00:29:34 +0300

    update1

---
+++ src/org/itstep/qa/lesson_two/runner/RunnerBush.java
@@ -0,0 +1,26 @@
+package org.itstep.qa.lesson_two.runner;
+
+import org.itstep.qa.lesson_two.zadanie.Bush;
+
+public class RunnerBush {
+    public static void main(String[] args) {
+        Bush bush_1 = new Bush(1,2,"Зеленый");
+        Bush bush_2 = new Bush(45,21,"Салатовый");
+        Bush bush_3 = new Bush(84,91,"Розовый");
+        Bush bush_4 = new Bush(66,33,"Красный");
+        Bush bush_5 = new Bush(10,25,"Пурпурный");
+        Bush bush_6 = new Bush(4,7,"Черный");
+        Bush bush_7 = new Bush(29,54,"Коричневый");
+        Bush bush_8 = new Bush(32,47,"Желтый");
+        Bush bush_9 = new Bush(89,54,"Синий");
+        Bush bush_10 = new Bush(63,44,"Фиолетовый");
+        Bush bush_11 = new Bush(90,25,"Оранжевый");
+        Bush bush_12 = new Bush(34,17,"Голубой");
+        Bush bush_13 = new Bush(65,51,"Малиновый");
+        Bush bush_14 = new Bush(55,9,"Перламутровый");
+        Bush bush_15 = new Bush(77,81,"Бежевый");
+        Bush bush_16 = new Bush(52,32,"Белый");
+        Bush bush_17 = new Bush(5,20,"Фуксия");
+        Bush bush_18 = new Bush(42,18,"Хаки");
+    }
+}
